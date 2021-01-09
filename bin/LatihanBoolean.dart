d4de90a1a2985e3b2f590ba98abe586213b71310
nurzainpradana <pradananurzain@gmail.com>
2021-01-09 11:45:12 +0700

    latihan boolean

---
+++ bin/LatihanBoolean.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments){
+  // Boolean
+  bool alwaysTrue = true;
+  var alwaysFalse = false;
+  var notTrue = !true;
+  bool notFalse = !false;
+
+  if(true) {
+    print("It's true");
+  } else {
+    print("It's False");
+  }
+}
